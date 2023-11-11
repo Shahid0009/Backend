2730c976b2062acd637c75e8410ffa4c6f84335b
Shahid <shahidahmed9019@gmail.com>
2023-11-11 23:25:15 +0530

    second

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-app.get('/', (req, res) => {
-res.send('Hello world')
-})
-app.get('/profile', (req, res) => {
-    res.send('Hello shahid')
-    })
-app.get('/profile/:username', (req, res) => {
-res.send(`hello my name is ${req.params.username}`)
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.get("/profile", (req, res) => {
+  res.send("Hello shahid");
+});
+app.get("/profile/:username", (req, res) => {
+  res.send(`hello my name is ${req.params.username}`);
+});
 
 app.listen(3000);
