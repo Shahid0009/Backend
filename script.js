a646fcbc4825a39c0ee05ce2b3c22737a4352cde
Shahid <shahidahmed9019@gmail.com>
2023-11-14 23:08:18 +0530

    ok

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index", {age:21});
 });
+
+/* 
+<---Code for Dynamic routing--->
 app.get("/profile", (req, res) => {
   res.send("Hello shahid");
 });
 app.get("/profile/:username", (req, res) => {
   res.send(`hello my name is ${req.params.username}`);
 });
+*/
 
 app.listen(3000);
